5765548cb8f59a3c5326b2a6c1502d9031dd1eac
henriw22 <henriw22@gmail.com>
2021-08-01 17:57:07 -0700

    back end setup

---
+++ express-back-end/src/db/schema/development.sql
@@ -0,0 +1,12 @@
+INSERT INTO users (name, email, password)
+VALUES
+('Elise Carter', 'rosalinda_mclaughlin@hotmail.com', 'password'),
+('Micheal Schmidt', 'wilkinson.timmothy@hotmail.com', 'password'),
+('Anderson Stokes', 'reichel_albina@stamm.info', 'password'),
+('Eunice Morar', 'danielle.aufderhar@kilback.net', 'password'),
+('Brian Jones', 'percy_ratke@cornelius.biz', 'password'),
+('Herminia Smitham', 'sawayn.sarina@yahoo.com', 'password'),
+('Darien Luettgen', 'issac_collins@pink.io', 'password'),
+('Irwin King', 'kody_pfannerstill@john.us', 'password'),
+('Stan Miller', 'mcdermott.maxie@schoen.com', 'password'),
+('Bernice Reichel', 'maximilian.powlowski@rod.name', 'password');
