434bb4bcfa429231327783ac07a8b6b9c775eeb0
YucenLyc <yucen.u@gmail.com>
2021-08-03 11:37:59 -0700

    add optional questions for client users on addNewCase page

--- react-front-end/src/pages/AddNewCase.jsx
+++ react-front-end/src/pages/AddNewCase.jsx
@@ -1,35 +1,51 @@
-import React from 'react';
-import Header from './Header';
+import React from "react";
+import Header from "./Header";
+import Form from "react-bootstrap/Form";
+import FloatingLabel from "react-bootstrap/esm/FloatingLabel";
 
-const AddNewCase =  () => {
-
-  return(
+const AddNewCase = () => {
+  return (
     <div>
       <Header />
       <form>
         <div class="form-group row">
-          <label class="col-sm-2 col-form-label">  Your Legal Name: </label>
+          <label class="col-sm-2 col-form-label">Your Legal Name: </label>
           <div class="col-sm-10">
             <input type="legalname" class="form-control" id="name" />
           </div>
         </div>
         <div class="form-group row">
-          <label class="col-sm-2 col-form-label">  Date(the date you became involved in the case): </label>
+          <label class="col-sm-2 col-form-label">
+            Date(the date you became involved in the case):{" "}
+          </label>
           <div class="col-sm-10">
             <input type="date" class="form-control" id="date" />
           </div>
         </div>
-        <div>
+        {/* <div>
           <textarea class="new-case-description"></textarea>
-        </div>
+        </div> */}
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Control type="textarea"  placeholder="Optional Questions: Are you currently invovled in a legal case?" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea2">
+            <Form.Control type="textarea" placeholder="Optional Questions: Are you a plaintiff or a defendant?" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea3">
+            <Form.Control as="textarea" placeholder="Please give a short and concise description of your legal dispute:" />
+          </Form.Group>
+        </Form>
         <div class="form-group row">
           <div class="col-sm-10">
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
           </div>
         </div>
       </form>
     </div>
   );
-}
+};
 
 export default AddNewCase;
