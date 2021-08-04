6fb3343a6647dc0a3840dc0da609966249e85335
YucenLyc <yucen.u@gmail.com>
2021-08-03 23:00:18 -0700

    accept frontend styling changes

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -8,39 +8,7 @@ import { BrowserRouter as Router,
   Link, 
   Redirect 
 } from "react-router-dom";
-// class App extends Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       message: 'Click the button to load data!'
-//     }
-//   }
 
-//   fetchData = () => {
-//     axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-//     .then((response) => {
-//       // handle success
-//       console.log(response.data) // The entire response from the Rails API
-
-//       console.log(response.data.message) // Just the message
-//       this.setState({
-//         message: response.data.message
-//       });
-//     }) 
-//   }
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1>{ this.state.message }</h1>
-//         <button onClick={this.fetchData} >
-//           Fetch Data
-//         </button>        
-//       </div>
-//     );
-//   }
-// }
-//Pages
 import LandingPage from './pages/LandingPage';
 import NotFoundPage from './pages/404';
 import clientRegister from './pages/clientRegister'
