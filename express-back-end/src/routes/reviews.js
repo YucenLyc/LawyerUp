39e6fd31a2355b066db90d70d988691fd2ee42f4
Henri William <83308210+henriw22@users.noreply.github.com>
2021-08-05 22:04:50 -0700

    revised reviews api route

--- express-back-end/src/routes/reviews.js
+++ express-back-end/src/routes/reviews.js
@@ -3,7 +3,9 @@ const router = require("express").Router();
 module.exports = db => {
   router.get("/reviews", (request, response) => {
     db.query(`
-      SELECT * from reviews;
+      SELECT reviews.id, users.name, date, content 
+      FROM reviews JOIN lawyers ON reviews.lawyer_id = lawyers.id
+      JOIN users on lawyers.user_id = users.id;
     `)
     .then(({ rows: reviews }) => {
       response.json(reviews);
