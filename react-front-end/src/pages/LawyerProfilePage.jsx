e5332898a3f8cf84a88ee6097f820b7f898906e0
Henri William <83308210+henriw22@users.noreply.github.com>
2021-08-07 13:22:25 -0700

    making the profile page data to pull the information dynamically form the db

--- react-front-end/src/pages/LawyerProfilePage.jsx
+++ react-front-end/src/pages/LawyerProfilePage.jsx
@@ -8,43 +8,40 @@ const axios = require('axios');
 export default function LawyerProfilePage() {
   const { lawyer_id } = useParams();
   const [lawyers, setLawyers] = useState([]);
-  const [filter, setFilter] = useState([]);
 
-  const fakeData = {"name":"Yucen", "specialization": "Business Law", "profile": "I am specialized in family law related legal issues"};
+
   useEffect(() => {
     axios.get("/api/lawyers").then(response => {
       setLawyers(response.data);
-      setFilter(response.data);
     });
   }, [])
+
   console.log('lawyers= ', lawyers);
+  console.log('lawyer_id', lawyer_id);
+
   const filterByLawyerId = (lawyer_id) => {
   
-    return filter.find(e => e.id == lawyer_id);
+    return lawyers.find(e => e.id === Number(lawyer_id)) || {};
     
   }
+  
+  const result = filterByLawyerId(lawyer_id);
 
-  // const result = filterByLawyerId(lawyer_id);
-  // console.log('result = ',result);
-  useEffect(() => {
-    setFilter(filterByLawyerId(lawyer_id));
-  }, []);
+  console.log('result: ', result);
+
   return (
     <>
     <Header />
     <div className="heading-container">
-      <h3 className="lawyer-name">{fakeData.name}</h3>
-      <br></br>
-      <h4 className="specialization">Specialization:</h4>
-        <p className="speciality-list">
-            {fakeData.specialization} 
-        </p>
-    </div>
-    <div className="content-container">
-      <h4>My Self</h4>
-      <textarea readOnly>{fakeData.profile}</textarea>
-      
-    </div>
+        <h3 className="lawyer-name">{result.name}</h3>
+        <br></br>
+        <h4 className="specialization">Specialization:</h4>
+        <div className="speciality-list">{result.speciality}</div>
+      </div>
+      <div className="content-container">
+        <h4>My Self</h4>
+        <p>{result.profile}</p>
+      </div>
     <br></br> 
     {/* <SummaryDisplay /> */}
    
