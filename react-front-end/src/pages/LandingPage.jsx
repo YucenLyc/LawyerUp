c004726af166cab6e34014ac4ac58108da632a00
Henri William <83308210+henriw22@users.noreply.github.com>
2021-08-08 11:00:09 -0700

    modified landing page

--- react-front-end/src/pages/LandingPage.jsx
+++ react-front-end/src/pages/LandingPage.jsx
@@ -1,16 +1,63 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { authContext } from '../providers/AuthProvider';
 import Header from './Header';
+import './LandingPage.scss';
+import { useHistory } from 'react-router-dom';
 
 const LandingPage = ({ history }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useContext(authContext);
+  const [user, setUser] = useState("");
+  // const history = useHistory();
+
+  const onPasswordChange = function (event) {
+    setPassword(event.target.value);
+  };
+
+  const onSubmit = function (event) {
+    event.preventDefault();
+    if (email === "Egg Eggerson") {
+      history.push('/clientHomePage');
+    }
+  };
+
 
   return(
-    <div>
-      <Header />
-      <div>
-        <button type="button" class="btn btn-primary btn-lg" onClick={() => history.push('/lawyerHome')}>Lawyer</button>
-        <button type="button" class="btn btn-primary btn-lg" onClick={() => history.push('/clientHome')}>Client</button>
+    <>
+      <Header value={user}/>
+      <div className="landing-body">
+        <div className="user-options">
+          <div className="user-type">
+            <button type="button" class="btn btn-primary btn-lg" onClick={() => history.push('/lawyerHome')}>Lawyer</button>
+            <button type="button" class="btn btn-primary btn-lg" onClick={() => history.push('/clientHome')}>Client</button>
+          </div>
+        </div>    
+        <div className="form-body">
+          <form className="login-form" onSubmit={onSubmit}>
+            <div>
+              <input type="text" name="login"
+                value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Username"
+                  />
+            </div>
+            <div>
+              <input type="password" name="password"
+                value={password} placeholder="Password"
+                onChange={onPasswordChange} />
+            </div>
+            <div className="buttons">
+              <div className="submit">
+                <input onClick={() => setUser(email)} type="submit" name="commit" value="Login" />
+              </div>
+              <div className="sign-up">
+                <input onClick={() => setUser(email)} type="submit" name="commit" value="Sign Up" />
+              </div>
+            </div>
+          </form>
+            {/* {user === false? <section verifyClient={verifyClient} /> : <div> </div>} */}
+        </div>
       </div>
-    </div>     
+    </> 
   );
 }
 
