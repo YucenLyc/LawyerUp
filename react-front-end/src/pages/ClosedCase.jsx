0ea003830f3c5552052d1b0a719c0083ee998829
YucenLyc <yucen.u@gmail.com>
2021-08-10 22:55:28 -0700

    color changes on the navbar/(WIP) for closed case

--- react-front-end/src/pages/ClosedCase.jsx
+++ react-front-end/src/pages/ClosedCase.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "../components/ClientHomeNavbar";
 
-const axios = require('axios');
+const axios = require("axios");
 
 // const fakeClientCase = {
 //   case_description: " My name is Egg Eggerson, I got into an argument with my landlord recently over my cat Smooj. When I signed the rental contract, the contract didn't address anything about tenant owning pets, so I assumed it's okay for me to have a cat. I've been living in this apartment for over a year now, and recently my landlord found out from the neighbours that I have a cat. She said I could either move out this month or get rid of Smooj. Of course getting rid of Smooj is not an option for me, so I want to know if she is allowed to make me move out under this circumstance. Please contact me, if you're experience in handling rental dispute!! Much thanks!!"
@@ -11,22 +11,22 @@ const axios = require('axios');
 
 function ClosedCase(props) {
   const { case_id } = useParams();
-  const [ cases, setCases] = useState([]);
+  const [cases, setCases] = useState([]);
   // const [isOpen, setIsOpen] = useState(false);
 
-  console.log('case id: ', case_id);
+  console.log("case id: ", case_id);
 
   useEffect(() => {
-    axios.get("/api/closedcases").then(response => {
+    axios.get("/api/closedcases").then((response) => {
       setCases(response.data);
     });
-  }, [])
+  }, []);
 
-  console.log('cases: ', cases);
+  console.log("cases: ", cases);
 
   const findCaseById = (id) => {
     return cases.find((elm) => elm.id === Number(id)) || {};
-  }
+  };
 
   const closedCase = findCaseById(case_id);
   console.log(closedCase);
@@ -36,14 +36,21 @@ function ClosedCase(props) {
   // }
 
   return (
-   
     <div>
       <Navbar />
+      <header>
+        <h1>
+          Closed <strong>Cases</strong> Detail
+        </h1>
+        <p>...I came, I saw, I conquered </p>
+      </header>
       <div class="caseInfo">
         <label for="caseId">Case ID: {closedCase.id}</label>
       </div>
       <div>
-        <label for="caseDate">Case Date: {new Date(closedCase.date).toDateString()}</label>
+        <label for="caseDate">
+          Case Date: {new Date(closedCase.date).toDateString()}
+        </label>
       </div>
       <div>
         <label for="clientName">Client Name: {closedCase.client_name}</label>
@@ -59,11 +66,8 @@ function ClosedCase(props) {
         <p>{closedCase.description}</p>
       </div>
       <br></br>
-      
-   
     </div>
-   
   );
-};
+}
 
 export default ClosedCase;
