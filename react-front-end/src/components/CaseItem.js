0a90ed1f2475d49483f6fcdf9ccdccd33a730025
YucenLyc <yucen.u@gmail.com>
2021-08-11 20:51:20 -0700

    add star for my case

--- react-front-end/src/components/CaseItem.js
+++ react-front-end/src/components/CaseItem.js
@@ -30,14 +30,8 @@ export default function CaseItem(props) {
 
 
   return (
-
-    
-    
     <main className="case-box" onClick={togglePopup}>
-       
-     
       <div>
-
         {isOpen && <CasePopup
           content={<>
             <h5>Case Id: {props.id}</h5>
@@ -59,15 +53,14 @@ export default function CaseItem(props) {
         {open && <CasePopup
           content={<AssignCase onCancel={() => setOpen(!open)} onAssign={assignCase} />}
           handleClose={() => setOpen(!open)}
-          />}
+        />}
         <section className="case-name">
           <div className="case-header">
             <h5><b>Client: {props.client}</b></h5>
             {(sessionStorage.name === props.client) &&
-                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-hourglass-bottom" viewBox="0 0 16 16">
-                 <path d="M2 1.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1h-11a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1-.5-.5zm2.5.5v1a3.5 3.5 0 0 0 1.989 3.158c.533.256 1.011.791 1.011 1.491v.702s.18.149.5.149.5-.15.5-.15v-.7c0-.701.478-1.236 1.011-1.492A3.5 3.5 0 0 0 11.5 3V2h-7z"/>
-               </svg>
-                
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+              </svg>
             }
           </div>
         </section>
@@ -84,8 +77,16 @@ export default function CaseItem(props) {
           </section>
         </section>
       </div>
-      
     </main>
-   
+    //  <div class="post">
+    //    <div class="post_header"></div>
+    //    <i class="material-icons sidebar__topAvatar">account_circle</i>
+    //    <div class="post__info">{props.name}</div>
+    //    <p></p>
+    //    <div class="post_body"></div>
+    //    <div class="feed__inputOptions">
+    //      <div class="inputOption"></div>
+    //    </div>
+    //  </div>
   )
 }
